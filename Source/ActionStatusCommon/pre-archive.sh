c492a9abc7be8164a92f2462c6ec051e1d91a021
Sam Deane <sam@elegantchaos.com>
2020-02-14 15:21:14 +0000

    moved tools, fixed build scripts

--- Source/ActionStatusCommon/pre-archive.sh
+++
@@ -1,3 +0,0 @@
-# update main build number
-cd "$PROJECT_DIR/.."
-swift run --package-path "Tools" rt update-build --config ActionStatus/Configs/BuildNumber.xcconfig> /tmp/prearchive.log
---
+++ Sources/ActionStatusCommon/pre-archive.sh
@@ -0,0 +1,3 @@
+# update main build number
+cd "$SOURCE_ROOT"
+swift run --package-path "$ACTION_STATUS_TOOLS_PATH" rt update-build --config "$ACTION_STATUS_COMMON_SOURCE_PATH/BuildNumber.xcconfig" > /tmp/prebuild.log
