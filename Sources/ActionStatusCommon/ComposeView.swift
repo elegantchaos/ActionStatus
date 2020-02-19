9b88d0a17660549a31369426d3bbed845c71ca2e
Sam Deane <sam@elegantchaos.com>
2020-02-19 16:17:45 +0000

    building out the configuration ui

--- Sources/ActionStatusCommon/ComposeView.swift
+++ Sources/ActionStatusCommon/ComposeView.swift
@@ -14,7 +14,7 @@ class Option: ObservableObject {
     init(_ id: String, name: String) {
         self.id = id
         self.name = name
-        self.included = false
+        self.included = true
     }
 }
 
@@ -25,9 +25,16 @@ extension Option: Equatable {
 }
 
 class Job: Option {
-    let swift: String?
+    enum Platform {
+        case mac
+        case linux
+    }
     
-    func yaml() -> String {
+    let swift: String?
+    let platform: Platform
+    let includeXcode: Bool
+    
+    func yaml(build: Bool, test: Bool, notify: Bool, package: String) -> String {
         var yaml =
             """
             \(id):
@@ -35,22 +42,104 @@ class Job: Option {
             
             """
         
-        if let swift = swift {
+        switch (platform) {
+            case .mac:
             yaml.append(
+            """
+                runs-on: macOS-latest
+
+            """
+            )
+            
+            case .linux:
+                let swift = self.swift ?? "5.1"
+                yaml.append(
             """
                 runs-on: ubunu-latest
                 container: swift:\(swift)
+
+            """
+                )
+        }
+        
+        yaml.append(
+            """
+                steps:
+                - name: Checkout
+                  uses: actions/checkout@v1
+                - name: Swift Version
+                  run: swift --version
+
+            """
+        )
+
+        if build {
+            yaml.append(
+            """
+                - name: Build
+                  run: swift build -v
+
             """
             )
         }
+
+        if test {
+            yaml.append(
+            """
+                - name: Test
+                  run: swift test -v
+
+            """
+            )
+        }
+
+        if includeXcode {
+            if build {
+                yaml.append(
+                """
+                    - name: Build (iOS)
+                      run: xcodebuild clean build -workspace . -scheme \(package) -destination "name=iPad Pro (11-inch)"
+
+                """
+                )
+            }
+
+            if test {
+                yaml.append(
+                """
+                    - name: Test (iOS)
+                      run: xcodebuild test -workspace . -scheme \(package) -destination "name=iPad Pro (11-inch)"
+
+                """
+                )
+            }
+        }
         
+        if notify {
+            yaml.append(
+            """
+                - name: Slack Notification
+                  uses: elegantchaos/slatify@master
+                  if: always()
+                  with:
+                    type: ${{ job.status }}
+                    job_name: '\(name)'
+                    mention_if: 'failure'
+                    url: ${{ secrets.SLACK_WEBHOOK }}
+
+            """
+            )
+        }
+
         yaml.append("\n\n")
         
         return yaml
     }
     
-    init(_ id: String, name: String, swift: String? = nil) {
+    init(_ id: String, name: String, platform: Platform = .linux, swift: String? = nil, includeXcode: Bool = false) {
+        self.platform = platform
         self.swift = swift
+        self.includeXcode = includeXcode
         super.init(id, name: name)
     }
 }
@@ -62,8 +151,8 @@ struct ComposeView: View {
     @State var workflow: String = "Tests"
     
     @State var platforms = [
-        Job("macOS", name: "macOS"),
-        Job("iOS", name: "iOS"),
+        Job("macOS", name: "macOS", platform: .mac),
+        Job("iOS", name: "iOS", platform: .mac),
         Job("linux-50", name: "Linux (Swift 5.0)", swift: "5.0"),
         Job("linux-51", name: "Linux (Swift 5.1)", swift: "5.1")
     ]
@@ -108,7 +197,13 @@ struct ComposeView: View {
                 Section(header: Text("Other Options")) {
                     VStack {
                         Toggle(isOn: $build) {
-                            Text("Build")
+                            Text("Perform Build")
+                        }
+                        Toggle(isOn: $test) {
+                            Text("Run Tests")
+                        }
+                        Toggle(isOn: $notify) {
+                            Text("Post Notification")
                         }
                     }
                 }
@@ -130,19 +225,24 @@ struct ComposeView: View {
     
     func enabledJobs() -> [Job] {
         var jobs: [Job] = []
-        var includeMac = false
+        var macOS = false
+        var iOS = false
         for platform in platforms {
             switch platform.id {
-                case "macOS", "iOS":
-                    includeMac = true
+                case "macOS":
+                    macOS = true
+                case "iOS":
+                    iOS = true
                 
                 default:
                     jobs.append(platform)
             }
         }
         
-        if includeMac {
-            jobs.append(Job("macOS-iOS", name: "macOS/iOS"))
+        if macOS || iOS {
+            jobs.append(
+                Job("macOS-iOS", name: "macOS/iOS", platform: .mac, includeXcode: iOS)
+            )
         }
         
         return jobs
@@ -163,7 +263,7 @@ struct ComposeView: View {
         """
         
         for job in enabledJobs() {
-            source.append(job.yaml())
+            source.append(job.yaml(build: build, test: test, notify: notify, package: "Test"))
         }
         
         print(source)
