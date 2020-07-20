ba3f24e574332a1f7a09630cad2ee638720d161a
Sam Deane <sam@elegantchaos.com>
2020-07-20 16:18:04 +0100

    Moved rt configs to new location.

---
+++ Sources/Tools/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tools2
+//
+//  Created by Sam Deane on 20/07/2020.
+//  Copyright © 2020 Elegant Chaos. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
