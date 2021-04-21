83ce915a8fbf74331d2e4ce3807e9f228c0901b7
Sam Deane <sam@elegantchaos.com>
2021-04-21 16:46:41 +0100

    Added some settings to the test model

--- Core/Sources/Core/TestModel.swift
+++ Core/Sources/Core/TestModel.swift
@@ -10,18 +10,21 @@ public class TestModel: Model {
         var datastoreRepo = Repo("Datastore", owner: "elegantchaos", workflow: "Swift", state: .passing)
         datastoreRepo.settings = WorkflowSettings(options: ["macOS", "iOS", "linux", "swift-52", "swift-53", "release", "build", "test", "header"])
         
+        var collection = Repo("CollectionExtensions", owner: "elegantchaos", workflow: "Tests", state: .passing)
+        collection.settings.options = ["swift-52", "swift-53", "swift-nightly", "macOS", "iOS", "linux", "release", "build", "test", "firstlast", "upload", "header"]
+        
         repos = [
             Repo("Actions", owner: "elegantchaos", workflow: "Tests", state: .passing),
             Repo("ActionsKit", owner: "elegantchaos", workflow: "Tests", state: .passing),
             Repo("ApplicationExtensions", owner: "elegantchaos", workflow: "Tests", state: .failing),
             Repo("BindingsExtensions", owner: "elegantchaos", workflow: "Tests", state: .passing),
             Repo("Builder", owner: "elegantchaos", workflow: "Tests", state: .passing),
-            Repo("CollectionExtensions", owner: "elegantchaos", workflow: "Tests", state: .passing),
+            collection,
             Repo("CommandShell", owner: "elegantchaos", workflow: "Tests", state: .passing),
             datastoreRepo,
             Repo("Hardware", owner: "elegantchaos", workflow: "Build", state: .failing),
-            Repo("Logger", owner: "elegantchaos", workflow: "tests", state: .unknown),
-            Repo("ViewExtensions", owner: "elegantchaos", workflow: "Tests", state: .passing),
+            Repo("Logger", owner: "elegantchaos", workflow: "tests", state: .running),
+            Repo("ViewExtensions", owner: "elegantchaos", workflow: "Tests", state: .queued),
         ]
 
         super.init(repos)
